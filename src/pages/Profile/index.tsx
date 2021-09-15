0add3a830c142446bbad96de707bda7cc7cf67e6
Raphael <rapha_rmm@hotmail.com>
2021-09-15 16:51:22 -0300

    ✨ Create Profile section

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
+import ProfileData from '../../components/ProfileData';
+
+import { Container, Main, LeftSide, RightSide } from './styles';
+
 const Profile: React.FC = () => {
-    return <div />;
+    return (
+        <Container>
+            <Main>
+                <LeftSide>
+                    <ProfileData 
+                        username={'bllackdev'}
+                        name={'Raphael Renato'}
+                        avatarUrl={'https://avatars.githubusercontent.com/u/42950800?v=4'}
+                        followers={68}
+                        following={124}
+                        company={'CI&T'}
+                        location={'São Paulo,Brazil'}
+                        email={'rapha_rmm@hotmail.com'}
+                        blog={'bllackdev.vercel.app'}
+                    />
+                </LeftSide>
+                <RightSide></RightSide>
+            </Main>
+        </Container>
+    );
 }
 
 export default Profile;
